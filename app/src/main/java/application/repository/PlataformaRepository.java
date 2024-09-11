d9186acaa4af8b67fee792442dd94b0b1aec76fc
Vinicius Machado <viniciusmach11@gmail.com>
2024-09-11 23:47:22 +0000

    Pg 22

---
+++ app/src/main/java/application/repository/PlataformaRepository.java
@@ -0,0 +1,9 @@
+package application.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import application.model.Plataforma;
+
+public interface PlataformaRepository extends CrudeRepository<Plataforma, Long> {
+
+}
